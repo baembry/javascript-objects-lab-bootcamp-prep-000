20976f0a0fa629903cbf14a6ac5ba66e17b2cbef
Brian Embry <b.a.embry@gmail.com>
2017-12-07 18:40:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var recipes = { }
 
-/*
+
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newObject = object.assign({ }, object, {key: value})
+  var newObject = Object.assign({ }, object, {key: value})
   return newObject
-} */
+} 
+
+/*
 function updateObjectWithKeyAndValue(object, key, value) {
   var newPair = {[key]: value}
   var newObject = Object.assign(object, newPair)
   return newObject
 }
+*/
