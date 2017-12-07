c2f4a77a8b45ca078c565ba556faf9277c8bb112
Brian Embry <b.a.embry@gmail.com>
2017-12-07 18:55:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return newObject
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  
+  object.key[value]
+  return object
 }
